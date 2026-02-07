e8eac3084c1d8a8d26fe23e787867203df9d3eb0
Trupti Sagar Patil <truptip1601@gmail.com>
2026-02-07 12:00:40 +0530

    Add files via upload

---
+++ ass3.js
@@ -0,0 +1,14 @@
+// Fix the Welcome Message
+alert("Welcome to Tony’s Café — Where Genius Meets Coffee!");
+
+// Ask the Customer’s Name
+let customerName = prompt("What’s your name, dear guest?");
+
+// Display Personalized Greeting
+alert("Hello, " + customerName + "! Enjoy your time at Tony’s Café ☕");
+
+
+// 💡 Bonus Challenge
+let favoriteDrink = prompt("What’s your favorite drink?");
+
+alert("Hello, " + customerName + "! Enjoy your " + favoriteDrink + " at Tony’s Café ☕");
