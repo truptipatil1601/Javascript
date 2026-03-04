ac1187e42b55370200a4b38df78e879d60f44d0e
Trupti Sagar Patil <truptip1601@gmail.com>
2026-03-04 23:22:08 +0530

    Add files via upload

---
+++ ass12.js
@@ -0,0 +1,19 @@
+// Let & Const 📦
+// Codédex
+
+const firstName = "Jane";
+const favoriteColor = "Purple 💜";
+
+let currentLocation = "New York 🗽";
+let mood = "Chill";
+
+console.log("My Profile: ");
+console.log(firstName);
+console.log(favoriteColor);
+console.log(currentLocation);
+console.log(mood);
+
+currentLocation = "Chicago 🌬️";
+mood = "Thrilled";
+console.log(currentLocation);
+console.log(mood);
