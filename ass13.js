8cb52a2c16d02e274e8756509f3169e5accd6084
Trupti Sagar Patil <truptip1601@gmail.com>
2026-03-05 19:52:53 +0530

    Add files via upload

---
+++ ass13.js
@@ -0,0 +1,12 @@
+// Data Types 📦
+// Codédex
+
+const companyName = "Niteowl, Inc.";
+const foundingYear = 2022;
+let isActive = true;
+let fundingAmount = undefined;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
